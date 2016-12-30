04aee256d57ad5cb6291c87ac48adeaa1e3b8ef9
huyang10 <huyang1@jd.com>
2016-12-30 19:05:58 +0800

    移动lib

--- src/com/shadow/util/HelloWorld.java
+++ src/com/shadow/util/HelloWorld.java
@@ -17,19 +17,39 @@ public class HelloWorld {
 	public static String cfFile = rootFile + "/gui-config.json";
 
 	public static void main(String[] args) throws IOException {
+		// IOUtil.closeExe();
+		// ShadowTotalBean shadowTotalBean = new ShadowTotalBean();
+		// ArrayList<ShadowInfoBean> beans = new ArrayList<ShadowInfoBean>();
+		// // ////////////////
+		// Ishadowsocks.parseInfo(beans);
+		// Freessr.parseInfo(beans);
+		// Doubishadowsocks.parseInfo(beans);
+		// // Freevpnss.parseInfo(beans);
+		// // //////////////////////////
+		// shadowTotalBean.setConfigs(beans);
+		// String text = JSON.toJSONString(shadowTotalBean);
+		// IOUtil.fileWriterHandle(cfFile, text);
+		// IOUtil.doExe(ssFile);
+		pcWork();
+	}
+
+	public static void pcWork() {
 		IOUtil.closeExe();
-		ShadowTotalBean shadowTotalBean = new ShadowTotalBean();
-		ArrayList<ShadowInfoBean> beans = new ArrayList<ShadowInfoBean>();
-		// ////////////////
-		Ishadowsocks.parseInfo(beans);
-		Freessr.parseInfo(beans);
-		Doubishadowsocks.parseInfo(beans);
-		// Freevpnss.parseInfo(beans);
-		// //////////////////////////
-		shadowTotalBean.setConfigs(beans);
+		ShadowTotalBean shadowTotalBean = getInfo();
 		String text = JSON.toJSONString(shadowTotalBean);
 		IOUtil.fileWriterHandle(cfFile, text);
 		IOUtil.doExe(ssFile);
+	}
+
+	public static ShadowTotalBean getInfo() {
+		ShadowTotalBean shadowTotalBean = new ShadowTotalBean();
+		ArrayList<ShadowInfoBean> beans = new ArrayList<ShadowInfoBean>();
+		Ishadowsocks.parseInfo(beans);
+		Freessr.parseInfo(beans);
+		Doubishadowsocks.parseInfo(beans);
+		Freevpnss.parseInfo(beans);
+		shadowTotalBean.setConfigs(beans);
+		return shadowTotalBean;
 	}
 
 }
